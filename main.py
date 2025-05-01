b73317f2f2013222e69d4bf3a9cf23591839e0b1
gongbu22 <pyujin0711@naver.com>
2025-05-01 16:53:34 +0900

    수정

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ react_port = os.getenv("REACT_APP_PORT", "3000")
 # CORS 설정
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[f"{react_host}:{react_port}"],  # React 앱이 동작하는 주소 (개발 중)
+    allow_origins=["*"],  # React 앱이 동작하는 주소 (개발 중)
     allow_credentials=True,
     allow_methods=["*"],  # 모든 HTTP 메서드 허용
     allow_headers=["*"],  # 모든 헤더 허용
