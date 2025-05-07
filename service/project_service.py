4ef47799d28e73895e2ffbf2c4353078f32a3c58
gongbu22 <pyujin0711@naver.com>
2025-05-07 14:29:04 +0900

    수정

--- service/project_service.py
+++ service/project_service.py
@@ -3,7 +3,7 @@ from models.projects import myportfolio
 
 async def get_all_projects() -> List[myportfolio]:
     projects = await myportfolio.find_all().to_list()
-    print(f"✅ find_all 결과: {projects}")
+    # print(f"✅ find_all 결과: {projects}")
     return projects
 
 async def get_detail(project_num: str) -> myportfolio:
